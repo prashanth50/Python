facf70dcbd52b2df96644b29b7fd2f1090dea42f
prashanth50 <47527111+prashanth50@users.noreply.github.com>
2019-02-14 11:22:11 +0530

    Delete p2.py

--- p2.py
+++
@@ -1,9 +0,0 @@
-import re
-pattern = re.compile(r'ab\W\d*')
-with open('p2.txt','r') as f:
-	contents = f.read()
-matches = pattern.findall(contents)
-print matches
-#for match in matches:
-#	print(match)	
-
